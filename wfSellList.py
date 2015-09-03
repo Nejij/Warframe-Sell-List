6ab48460bc31f390653449c903173b496ae56b12
nejij <poor9163@gmail.com>
2015-09-03 16:18:20 -0600

    added functions to increase readability and modularization

--- wfSellList.py
+++ wfSellList.py
@@ -13,6 +13,17 @@ def findMidPrice(table):
 	result=re.search('column-3">(.*)</td><t', table)
 	return result.group(1)
 	
+#converts bytes to string
+def toString(byteStr):
+	charStr=byteStr.decode("utf-8")
+	return charStr
+	
+#remove substring from string
+def remSub(mainStr, subStrArr):
+	for i, j in subStrArr.items():
+		mainStr=mainStr.replace(i, j)
+	return mainStr
+	
 #list of urls to check
 urls=['http://wftrading.net/sets/','http://wftrading.net/archwings-aw-weapons/','http://wftrading.net/void-trader-items/','http://wftrading.net/syndicate-weapons-mods','http://wftrading.net/prime-weapons/','http://wftrading.net/prime-warframes/','http://wftrading.net/warframe/','http://wftrading.net/rifle/','http://wftrading.net/shotgun/','http://wftrading.net/pistol/','http://wftrading.net/melee/','http://wftrading.net/sentinel/','http://wftrading.net/aura/','http://wftrading.net/most-wanted-rare/','http://wftrading.net/event-mods']
 	
@@ -37,16 +48,14 @@ for url in urls[:-1]:
 	#check if each line in the local file is in the html
 	for line in myItems:
 		#remove \r\n from the items
-		item=line[:len(line)-2]
+		item=line[:len(line)-2] #type: bytes
 		#convert from byte array to string
-		tmpItem=item.decode("utf-8")
+		tempStrItem=toString(item)
 		#remove "'" and "Blueprint"
-		newTmpItem=tmpItem.replace("'","")
-		del tmpItem
-		endItem=newTmpItem.replace(" Blueprint","")
+		strItem=remSub(tempStrItem, {"'":"", "Blueprint":""})
 		#if the line is in the html
 		if html.find(item)!=-1:
-			itemPrice=repr(endItem).replace("'","") + " " + repr(findMidPrice(html[html.find(item):html.find(item)+200].decode())).replace("'","") + "|"
+			itemPrice=strItem + " " + repr(findMidPrice(html[html.find(item):html.find(item)+200].decode())).replace("'","") + "|"
 			sellList.write(itemPrice)
 #close files
 myItems.close()
