a202caec109798b93d8692d3d661839285d035f4
nejij <poor9163@gmail.com>
2015-09-03 17:00:36 -0600

    add functions to get item price for further improved readability

--- wfSellList.py
+++ wfSellList.py
@@ -2,6 +2,9 @@ import urllib.request
 import re
 import os
 
+#######################################
+#              FUNCTIONS
+#######################################
 #get html from page
 def getHtml(url):
 	with urllib.request.urlopen(url) as response:
@@ -24,9 +27,9 @@ def remSub(mainStr, subStrArr):
 		mainStr=mainStr.replace(i, j)
 	return mainStr
 	
-#list of urls to check
-urls=['http://wftrading.net/sets/','http://wftrading.net/archwings-aw-weapons/','http://wftrading.net/void-trader-items/','http://wftrading.net/syndicate-weapons-mods','http://wftrading.net/prime-weapons/','http://wftrading.net/prime-warframes/','http://wftrading.net/warframe/','http://wftrading.net/rifle/','http://wftrading.net/shotgun/','http://wftrading.net/pistol/','http://wftrading.net/melee/','http://wftrading.net/sentinel/','http://wftrading.net/aura/','http://wftrading.net/most-wanted-rare/','http://wftrading.net/event-mods']
-	
+#######################################
+#                MAIN
+#######################################
 #delete old sell list
 try:
 	os.remove("sellList.txt")
@@ -36,6 +39,8 @@ except OSError:
 sellList=open("sellList.txt", "w+")
 #print WTS to file
 sellList.write("WTS|")
+#list of urls to check
+urls=['http://wftrading.net/sets/','http://wftrading.net/archwings-aw-weapons/','http://wftrading.net/void-trader-items/','http://wftrading.net/syndicate-weapons-mods','http://wftrading.net/prime-weapons/','http://wftrading.net/prime-warframes/','http://wftrading.net/warframe/','http://wftrading.net/rifle/','http://wftrading.net/shotgun/','http://wftrading.net/pistol/','http://wftrading.net/melee/','http://wftrading.net/sentinel/','http://wftrading.net/aura/','http://wftrading.net/most-wanted-rare/','http://wftrading.net/event-mods']
 #get html from each url
 for url in urls[:-1]:
 	html=getHtml(url)
@@ -47,16 +52,24 @@ for url in urls[:-1]:
 		print('Error: "myItems.txt" not found.')
 	#check if each line in the local file is in the html
 	for line in myItems:
-		#remove \r\n from the items
-		item=line[:len(line)-2] #type: bytes
+		#create item object and remove \r\n from the items
+		item=line[:len(line)-2]
 		#convert from byte array to string
 		tempStrItem=toString(item)
 		#remove "'" and "Blueprint"
-		strItem=remSub(tempStrItem, {"'":"", "Blueprint":""})
+		strItem=remSub(tempStrItem, {"'":"", " Blueprint":""})
 		#if the line is in the html
 		if html.find(item)!=-1:
-			itemPrice=strItem + " " + repr(findMidPrice(html[html.find(item):html.find(item)+200].decode())).replace("'","") + "|"
-			sellList.write(itemPrice)
+			#get the html for that item
+			itemHtmlStr=toString(html[html.find(item):html.find(item)+200])
+			#get mid price of item
+			tempItemPrice=findMidPrice(itemHtmlStr)
+			#remove "'"
+			itemPrice=remSub(tempItemPrice, {"'":""})
+			#create item with price
+			itemWithPrice=strItem + " " + itemPrice + "|"
+			#append item and price to file
+			sellList.write(itemWithPrice)
 #close files
 myItems.close()
 sellList.close()
